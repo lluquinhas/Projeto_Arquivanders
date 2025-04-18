ee6834f1041793c740de65987f63e1e3d5e69b3d
Lucas Miqueloti <xbrlucasx@gmail.com>
2025-04-18 11:21:29 -0300

    Primeiros Passos

---
+++ index/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Arquivander´s - Biblioteca Virtual</title>
+    <link rel="stylesheet" href="index.css">
+</head>
+<body>
+    <h1>Bem-Vindo á Biblioteca Virtual</h1>
+    <a href="login.php">Login</a>
+    <a href="cadastro.php">Cadastrar-se</a>
+</body>
+</html>
